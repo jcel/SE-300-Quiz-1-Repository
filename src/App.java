e3ce32684870bafe7ee7cdc8971c42e3ee05b1a0
jcel <c16james.combs@usafa.edu>
2015-10-05 15:43:55 -0400

    Team Leader Commit

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@
 public class App {
 	public static void main(String[] args){
 		System.out.println("Hello world!");
-		System.out.println("Hey there!!")
+		System.out.println("Hey there!!");
+		System.out.println("Now we are done");
 	}
 }
